892773433351b9b5e3b4ce2b88f8b0e0b331f600
DGuabiraba <daviguabiraba00@gmaill.com>
2025-07-01 11:43:49 -0300

    kjdflj

---
+++ aula05/src/aula05/Elefante.java
@@ -0,0 +1,16 @@
+package aula05;
+
+public class Elefante {
+	String cor ;
+	String nome;
+	public Elefante() {
+		
+	}
+	public Elefante(String nome ) {
+		this.nome =nome;
+	}
+	final void comer() {
+		System.out.println("comendo");
+	}
+
+}
